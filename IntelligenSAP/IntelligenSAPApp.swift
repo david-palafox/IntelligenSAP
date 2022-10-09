af4c1124ba3296a88d5584a0f724b4971298016e
David Palafox <david.palafox@gmail.com>
2022-10-09 17:49:09 -0500

    Initial Commit

---
+++ IntelligenSAP/IntelligenSAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligenSAPApp.swift
+//  IntelligenSAP
+//
+//  Created by David Palafox on 09/10/22.
+//
+
+import SwiftUI
+
+@main
+struct IntelligenSAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
